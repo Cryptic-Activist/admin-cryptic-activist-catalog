164aa23252cfaabb9bc3fb64eafccb541e6dff60
Davi Silva <davi.cunha.silva.ca@gmail.com>
2023-02-09 23:51:44 +0100

    .

--- src/services/axios/index.ts
+++ src/services/axios/index.ts
@@ -1,15 +1,11 @@
-import axios, { type AxiosRequestHeaders } from "axios";
+import axios, { type AxiosRequestHeaders } from 'axios';
 
 export const fetchGet = async (
 	endpoint: string,
 	headers?: AxiosRequestHeaders | any
 ) => {
-	try {
-		const response = axios.get(endpoint, { headers });
-		return await response;
-	} catch (error) {
-		throw error;
-	}
+	const response = axios.get(endpoint, { headers });
+	return await response;
 };
 
 export const fetchPost = async (
@@ -17,28 +13,16 @@ export const fetchPost = async (
 	body?: object,
 	headers?: AxiosRequestHeaders | any
 ) => {
-	try {
-		const response = axios.post(endpoint, body, { headers });
-		return await response;
-	} catch (error) {
-		throw error;
-	}
+	const response = axios.post(endpoint, body, { headers });
+	return await response;
 };
 
 export const fetchPut = async (endpoint: string, params: object) => {
-	try {
-		const response = axios.put(endpoint, params);
-		return await response;
-	} catch (error) {
-		throw error;
-	}
+	const response = axios.put(endpoint, params);
+	return await response;
 };
 
 export const fetchDelete = async (endpoint: string, params: object) => {
-	try {
-		const response = axios.delete(endpoint, params);
-		return await response;
-	} catch (error) {
-		throw error;
-	}
+	const response = axios.delete(endpoint, params);
+	return await response;
 };
